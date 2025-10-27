77b5050c1c8969d99839937ea71496485eb8cf49
TUFnarayan <125310038+TUFnarayan@users.noreply.github.com>
2025-10-28 04:32:26 +0530

    Add files via upload

---
+++ snowflake.sql
@@ -0,0 +1,94 @@
+-- Use DB & Schema
+USE DATABASE SNOWFLAKE_LEARNING_DB;
+USE SCHEMA PUBLIC;
+
+-- File format for JSON array
+CREATE OR REPLACE FILE FORMAT ff_json_array TYPE = JSON STRIP_OUTER_ARRAY = TRUE;
+
+-- Raw table
+CREATE OR REPLACE TABLE raw_store_json (
+  rec VARIANT,
+  src_filename STRING,
+  file_row_number INT,
+  load_ts TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
+);
+
+-- Load JSON from S3 stage
+COPY INTO raw_store_json (rec, src_filename, file_row_number)
+FROM (
+  SELECT $1, METADATA$FILENAME, METADATA$FILE_ROW_NUMBER
+  FROM @aws_stage_data_pipeline/store_products.json (FILE_FORMAT => ff_json_array)
+)
+ON_ERROR = 'CONTINUE';
+
+-- Clean view (wide format)
+CREATE OR REPLACE VIEW vw_store_clean AS
+SELECT
+  LPAD(TRIM(rec:" 08075"::string), 5, '0') AS zip_new,
+  UPPER(TRIM(rec:" NJ"::string)) AS state_new,
+  TRIM(INITCAP(rec:"120 jefferson st."::string)) AS address_new,
+  TRIM(INITCAP(rec:"Doe"::string)) AS last_name_new,
+  TRIM(INITCAP(rec:"John"::string)) AS first_name_new,
+  TRIM(INITCAP(rec:"Riverside"::string)) AS city_new,
+  src_filename, file_row_number, load_ts
+FROM raw_store_json;
+
+
+CREATE OR REPLACE VIEW vw_store_simple AS
+SELECT
+  LPAD(TRIM(rec:" 08075"::string), 5, '0') AS zip_new,
+  UPPER(TRIM(rec:" NJ"::string))           AS state_new,
+  TRIM(rec:"120 jefferson st."::string)    AS address_new,
+  TRIM(rec:"Doe"::string)                  AS last_name_new,
+  TRIM(rec:"John"::string)                 AS first_name_new,
+  TRIM(rec:"Riverside"::string)            AS city_new
+FROM raw_store_json;
+
+
+SELECT *
+FROM vw_store_simple
+LIMIT 10;
+
+
+
+
+
+-- File format for JSON array
+CREATE OR REPLACE FILE FORMAT ff_json_array TYPE = JSON STRIP_OUTER_ARRAY = TRUE;
+
+-- Raw table for TFL Tube Status
+CREATE OR REPLACE TABLE raw_tfl_tube_status (
+  rec VARIANT,
+  src_filename STRING,
+  file_row_number INT,
+  load_ts TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
+);
+
+
+COPY INTO raw_tfl_tube_status (rec, src_filename, file_row_number)
+FROM (
+  SELECT $1, METADATA$FILENAME, METADATA$FILE_ROW_NUMBER
+  FROM @aws_stage_data_pipeline/tube_status.json (FILE_FORMAT => ff_json_array)
+)
+ON_ERROR = 'CONTINUE';
+
+
+CREATE OR REPLACE VIEW vw_tfl_tube_status_simple AS
+SELECT
+  rec:id::string                                  AS line_id,
+  rec:name::string                                AS line_name,
+  rec:modeName::string                            AS mode_name,
+  rec:lineStatuses[0].statusSeverity::int         AS status_severity,
+  rec:lineStatuses[0].statusSeverityDescription::string AS status_desc,
+  rec:lineStatuses[0].reason::string              AS reason,
+  TRY_TO_TIMESTAMP_NTZ(rec:lineStatuses[0].validityPeriods[0].fromDate::string) AS from_utc,
+  TRY_TO_TIMESTAMP_NTZ(rec:lineStatuses[0].validityPeriods[0].toDate::string)   AS to_utc,
+  COALESCE(rec:lineStatuses[0].validityPeriods[0].isNow::boolean, FALSE)        AS is_now
+FROM raw_tfl_tube_status;
+
+
+
+
+
+
+SELECT * FROM vw_tfl_tube_status_simple LIMIT 10;
